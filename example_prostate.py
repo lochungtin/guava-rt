6cef7746249debd9fb8c0f2cc6b4a78aaa1c890e
lochungtin <lochungtin@gmail.com>
2026-03-17 18:16:15 +0000

    make spec file and move examples

--- example_prostate.py
+++ example_prostate.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     OBJ = ["bladder", "prostate", "rectum", "femoral_head_l", "femoral_head_r"]
 
     load = lambda x: [
-        Mask(np.load(join("..", "data", f"{x}_{obj}.npy")).transpose(2, 1, 0))
-        for obj in OBJ
+        Mask(np.load(join("data", f"{x}_{obj}.npy")).transpose(2, 1, 0)) for obj in OBJ
     ]
 
     masksU = load("prostate_upright")
@@ -29,27 +28,21 @@ if __name__ == "__main__":
         t = perf_counter()
         V = list(comp.getVolDiff(diff_only=False).values())
         labels = ["vol diff", "upright Vol", "supine Vol"]
-        print(
-            f"\n1. volume diff ========================> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n1. volume diff ========================> (t = {perf_counter() - t:.3f}s)")
         prettyPrintTable(OBJ[:3], V, labels)
 
     if True:
         t = perf_counter()
         SA = list(comp.getSADiff(diff_only=False).values())
         labels = ["sa diff", "upright sa", "supine sa"]
-        print(
-            f"\n2. surface area diff ==================> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n2. surface area diff ==================> (t = {perf_counter() - t:.3f}s)")
         prettyPrintTable(OBJ[:3], SA, labels)
 
     if True:
         t = perf_counter()
         DISP = list(comp.getROIDisplacementDiff().values())
         labels = ["L(-ve)/R(+ve)", "A(-ve)/P(+ve)", "I(-ve)/S(+ve)", "Magnitude"]
-        print(
-            f"\n3. roi displacement ===================> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n3. roi displacement ===================> (t = {perf_counter() - t:.3f}s)")
         prettyPrintTable(OBJ[:3], DISP, labels)
 
     if True:
@@ -57,17 +50,13 @@ if __name__ == "__main__":
         ASD = list(comp.getBSDDiff("asd").values())
         HD95 = list(comp.getBSDDiff("hd95").values())
         HD = list(comp.getBSDDiff("hd").values())
-        print(
-            f"\n4. bidirectional surface discrepancy ==> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n4. bidirectional surface discrepancy ==> (t = {perf_counter() - t:.3f}s)")
         prettyPrintTable(OBJ[:3], [ASD, HD95, HD], ["ASD", "HD95", "HD"])
 
     if True:
         t = perf_counter()
         data = comp.getSeparationDistanceDiff("volume")
-        print(
-            f"\n5. separation distance (vol) ===========> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n5. separation distance (vol) ===========> (t = {perf_counter() - t:.3f}s)")
         for loc, dat in data.items():
             print(f"\n\t[{loc}]")
             prettyPrintTable(["diff", "upright", "supine"], dat, SERIES_ANALYSIS_LABELS)
@@ -75,9 +64,7 @@ if __name__ == "__main__":
     if True:
         t = perf_counter()
         data = comp.getSeparationDistanceDiff("surface")
-        print(
-            f"\n6. separation distance (surface) =======> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n6. separation distance (surface) =======> (t = {perf_counter() - t:.3f}s)")
         for loc, dat in data.items():
             print(f"\n\t[{loc}]")
             prettyPrintTable(["diff", "upright", "supine"], dat, SERIES_ANALYSIS_LABELS)
@@ -85,9 +72,7 @@ if __name__ == "__main__":
     if True:
         t = perf_counter()
         data = comp.getSeparationDistanceDiff("rcvs")
-        print(
-            f"\n7. separation distance (rcvs) ==========> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n7. separation distance (rcvs) ==========> (t = {perf_counter() - t:.3f}s)")
         for loc, dat in data.items():
             print(f"\n\t[{loc}]")
             prettyPrintTable(["diff", "upright", "supine"], dat, SERIES_ANALYSIS_LABELS)
@@ -95,9 +80,7 @@ if __name__ == "__main__":
     if True:
         t = perf_counter()
         data = comp.getPercentageOverlapDiff("volume")
-        print(
-            f"\n8. percentage overlap (volume) =========> (t = {perf_counter() - t:.3f}s)"
-        )
+        print(f"\n8. percentage overlap (volume) =========> (t = {perf_counter() - t:.3f}s)")
         for loc, dat in data.items():
             print(f"\n\t[{loc}]")
             prettyPrintTable(["diff", "upright", "supine"], dat, SERIES_ANALYSIS_LABELS)
