60d0d2b3467957732dfd97b5063b7b1c0c74ec40
lochungtin <lochungtin@gmail.com>
2026-03-22 06:28:31 +0000

    fix: remove bare imports

--- src/guava_rt/__init__.py
+++ src/guava_rt/__init__.py
@@ -1,9 +1,9 @@
 from ._version import __version__
 
-from mask import Mask
-from region import Region
-from metrics import Metrics
-from utils import prettyPrintTable, seriesAnalysis, SERIES_ANALYSIS_LABELS
+from .mask import Mask
+from .region import Region
+from .metrics import Metrics
+from .utils import prettyPrintTable, seriesAnalysis, SERIES_ANALYSIS_LABELS
 
 __all__ = [
     "__version__",
